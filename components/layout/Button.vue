324fbdbdacfcefc2a60172b2d2962c515775064f
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-03-15 15:29:30 +0100

    feat: add basic components to layout

---
+++ components/layout/Button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button :class="[`btn`, `btn__${variant}`]">
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+type ButtonVariant = 'primary' | 'secondary'
+
+withDefaults(
+  defineProps<{
+    label: string
+    variant: ButtonVariant
+  }>(),
+  { variant: 'primary' },
+)
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  padding: 0;
+  outline: none;
+  border: none;
+  height: 32px;
+  width: 200px;
+  border-radius: 4px;
+  font-family: $textFont;
+  font-size: rem(14);
+  transition: background-color 200ms ease-in-out;
+
+  &__primary {
+    color: $white-color;
+    background-color: $unnamed-color-e1a044;
+
+    &:hover {
+      background-color: $gray-color-300;
+      cursor: pointer;
+    }
+  }
+
+  &__secondary {
+    color: $black-color;
+    background-color: $white-color;
+
+    &:hover {
+      background-color: $gray-color-300;
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ components/layout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <span :class="[`header`, `header__${variant}`]">
+    <slot />
+  </span>
+</template>
+
+<script lang="ts" setup>
+type HeaderVariant = 'white' | 'black'
+
+withDefaults(
+  defineProps<{
+    variant: HeaderVariant
+  }>(),
+  { variant: 'white' },
+)
+</script>
+
+<style lang="scss" scoped>
+.header {
+  font-size: rem(26);
+  font-weight: bold;
+  text-align: center;
+
+  &__white {
+    color: $white-color;
+  }
+
+  &__black {
+    color: $black-color;
+  }
+}
+</style>
