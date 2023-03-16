800367c28ec2cec871aa2244fe906d8956023cdf
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-03-16 14:17:26 +0100

    feat: add Banner components

---
+++ components/home/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <nuxt-link class="card" :to="link">
+    <div class="card__container" :style="{ backgroundImage: `url(${backgroundSrc})` }">
+      <LayoutHeader class="card__title">{{ title }}</LayoutHeader>
+    </div>
+  </nuxt-link>
+</template>
+
+<script lang="ts" setup>
+export interface CardProps {
+  title: string
+  backgroundSrc: string
+  link: string
+}
+
+defineProps<CardProps>()
+</script>
+
+<style lang="scss" scoped>
+.card {
+  flex: 1;
+  text-decoration: none;
+
+  &__container {
+    display: flex;
+    align-items: end;
+    padding: 50px;
+    height: 100%;
+    @include background-image-basic;
+  }
+
+  &__title {
+    width: 50%;
+    text-align: left;
+  }
+}
+</style>
