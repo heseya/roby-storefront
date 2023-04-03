d481c72971ae017161e246966e856823343d8072
Mateusz Krasik <mateusz.krasik@escolasoft.com>
2023-04-03 08:37:55 +0200

    feat: work with layout

---
+++ layouts/empty.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout-container {
+  background-color: #00000080;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/basket/authentication.vue
+++ pages/basket/authentication.vue
@@ -4,11 +4,11 @@
       <h2>Zaloguj się</h2>
       <div>
         Adres e-mail
-        <FormInput />
+        <FormInput name="email" />
       </div>
       <div>
         Hasło
-        <FormInput />
+        <FormInput name="password" />
       </div>
       <div>Nie pamiętasz hasła ?</div>
       <button>Zaloguj się</button>
@@ -24,7 +24,16 @@
     </div>
   </div>
 </template>
-<script lang="ts" setup>
-definePageMeta({ layout: false })
+<script setup lang="ts">
+definePageMeta({ layout: 'empty' })
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.basket-authentication-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+  background-color: #f7f7f7;
+  width: 40%;
+  padding: 25px;
+}
+</style>
