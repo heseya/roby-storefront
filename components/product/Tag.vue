85485bde70ff76a3e884eb956217d94331815994
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-04-12 11:57:12 +0200

    fix: color of text in tags

--- components/product/Tag.vue
+++ components/product/Tag.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="tag" :class="[`tag--${type}`, `tag--${size}`]" :style="{ backgroundColor: color }">
+  <div
+    class="tag"
+    :class="[`tag--${type}`, `tag--${size}`, { 'tag--dark-text': isDarkText }]"
+    :style="{ backgroundColor: color }"
+  >
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
-withDefaults(
+const props = withDefaults(
   defineProps<{
     color?: string
     type?: 'default' | 'sale'
@@ -17,6 +21,17 @@ withDefaults(
     size: 'default',
   },
 )
+
+const isDarkText = computed(() => {
+  if (!props.color) return false
+  const [red, green, blue] = props.color // Hex color e.g. #ff0000
+    .replace('#', '')
+    .match(/.{1,2}/g)
+    ?.map((x) => parseInt(x, 16)) || [0, 0, 0]
+
+  // Formula from https://stackoverflow.com/a/3943023
+  return red * 0.299 + green * 0.587 + blue * 0.114 > 186
+})
 </script>
 
 <style lang="scss" scoped>
@@ -26,6 +41,11 @@ withDefaults(
   font-size: rem(10);
   text-transform: uppercase;
   margin-bottom: 3px;
+  color: #fff;
+
+  &--dark-text {
+    color: $text-color;
+  }
 
   &--sale {
     background-color: $green-color;
