a6731d4e6d47646810222acf76a1ac768fb9e426
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-04-17 13:28:14 +0200

    fix: revert auth in cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -58,10 +58,6 @@ const isCartEmpty = computed(() => cart.length === 0)
 useHead({
   title: t('cart.title'),
 })
-
-definePageMeta({
-  middleware: ['auth'],
-})
 </script>
 
 <style lang="scss" scoped>
