2a8b92be3f22fbba91972bf69ce8479392269f53
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-04-17 16:27:40 +0200

    fix: refactor component

---
+++ pages/search/[query].vue
@@ -0,0 +1,33 @@
+<template>
+  <BaseContainer>
+    <ProductListPage :title="query" :sets="preparedSets" :query-params="{ search: query }" />
+  </BaseContainer>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const t = useLocalI18n()
+
+const { query } = route.params
+const { set } = route.query
+const preparedSets = computed(() => {
+  if (!set) {
+    return
+  }
+
+  return Array.isArray(set) ? set : [set]
+})
+
+useHead({
+  title: query,
+})
+
+useBreadcrumbs([
+  {
+    label: query,
+    link: ``,
+  },
+])
+</script>
+
+<style lang="scss" scoped></style>
