48f7ab6b791bfc0af8c6fbb7bf8a121a2a1d5a43
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-04-25 10:59:48 +0200

    feat: upgrade icons to responsive

---
+++ components/rent/advantages/Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="advantage">xd</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.advantages {
+  &__title {
+    font-size: rem(30);
+    text-align: left;
+  }
+}
+</style>
