41da19b54d7781c22eb197ed2f97e60fa1c13bd5
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-04-28 13:18:17 +0200

    fix: cr improvements

--- utils/product.ts
+++ utils/product.ts
@@ -1,7 +1,8 @@
 import { ProductAttribute } from '@heseya/store-core'
 import { PRODUCT_SUBTEXT_ATTRIBUTE_NAME } from '~/consts/subtextAttribute'
 
-export const getProductSubtext = <T>(item?: T & { attributes: ProductAttribute[] }) =>
-  item?.attributes.find(
+// TODO: do not attribute from fixed string
+export const getProductSubtext = <T extends { attributes: ProductAttribute[] }>(item?: T) =>
+  item?.attributes?.find(
     (attribute: ProductAttribute) => attribute.name === PRODUCT_SUBTEXT_ATTRIBUTE_NAME,
   )?.selected_options[0]?.name
