060ee8ea152a9a8b0402f0c869e4f0d4cdb48ad4
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-05-05 13:40:35 +0200

    fix: proper server side seo

--- composables/useSeo.ts
+++ composables/useSeo.ts
@@ -1,10 +1,10 @@
-import { SeoMetadata, getSeoValues } from '@heseya/store-core'
+import { SeoMetadata, createSeoMetatags, getSeoValues } from '@heseya/store-core'
 import { MaybeRef } from '@vueuse/core'
 import { useConfigStore } from '~/store/config'
 
 export const useSeo = (getSeoArray: () => Array<MaybeRef<SeoMetadata> | null | undefined>) => {
   const config = useConfigStore()
-  const { t: $t } = useI18n({ useScope: 'global' })
+  const { t } = useI18n({ useScope: 'global' })
 
   const inputSeo = computed(() => getSeoArray().filter(Boolean).map(unref) as SeoMetadata[])
 
@@ -12,15 +12,9 @@ export const useSeo = (getSeoArray: () => Array<MaybeRef<SeoMetadata> | null | u
     getSeoValues(...inputSeo.value, {
       ...config.seo,
       // Default title is used as suffix
-      title: $t('seo.loading'),
+      title: t('seo.loading'),
     }),
   )
 
-  useSeoMeta({
-    title: () => computedSeo.value.title || '',
-    description: () => computedSeo.value?.description || '',
-    ogImage: () => computedSeo.value?.og_image?.url || '',
-    twitterCard: () => computedSeo.value?.twitter_card || 'summary',
-    robots: () => (computedSeo.value?.no_index ? 'no_index' : 'index'),
-  })
+  useHead(() => createSeoMetatags(computedSeo.value))
 }
