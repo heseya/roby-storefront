4de020c922582c546bbfab24ef3f7d1c6b235bb3
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-05-05 12:56:46 +0200

    feat: manual control over cache

---
+++ server/middleware/cache.ts
@@ -0,0 +1,16 @@
+import { ACCESS_TOKEN_KEY, IDENTITY_TOKEN_KEY, REFRESH_TOKEN_KEY } from '~/consts/cookiesKeys'
+
+export default defineEventHandler((event) => {
+  const cookies = parseCookies(event)
+  const isAuthRequest = [ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, IDENTITY_TOKEN_KEY].some(
+    (key) => !!cookies[key],
+  )
+
+  setResponseHeader(event, 'Is-Auth', isAuthRequest.toString())
+
+  if (isAuthRequest) {
+    setResponseHeader(event, 'Cache-Control', 'no-store, max-age=0')
+  } else {
+    setResponseHeader(event, 'Cache-Control', 's-maxage=15, stale-while-revalidate')
+  }
+})
