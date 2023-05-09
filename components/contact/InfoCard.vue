8ad906cf1a84248ab1c077cb5e1dc5f9b2938415
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-05-09 12:48:27 +0200

    feat: add layout & contact info

---
+++ components/contact/InfoCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="info-card">
+    <span class="info-card__title">{{ data.name }}</span>
+    <span class="info-card__email">{{ data.email }}</span>
+    <span v-for="(number, index) in data.phones" :key="index">{{ number }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+export interface InfoCardProps {
+  name: string
+  email: string
+  phones: string[]
+}
+
+defineProps<{ data: InfoCardProps }>()
+</script>
+
+<style lang="scss" scoped>
+.info-card {
+  flex: 1;
+  @include flex-column;
+  padding: 12px 16px;
+  border: solid 1px $gray-color-300;
+  border-radius: 4px;
+
+  &__title {
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+
+  &__email {
+    color: var(--primary-color);
+  }
+}
+</style>
---
+++ pages/kontakt.vue
@@ -0,0 +1,76 @@
+<template>
+  <NuxtLayout>
+    <LayoutBreadcrumpsProvider :breadcrumbs="breadcrumb" />
+    <BaseContainer class="contact">
+      <div>
+        <div class="contact__header">
+          <LayoutHeader class="contact__title" variant="black" tag="h1">
+            ROBICAN S.C.R. JASTRZĘBSKI I WSPÓLNICY
+          </LayoutHeader>
+          <span>UL. ZAKOPIAŃSKA 190, 60-467 POZNAŃ</span>
+        </div>
+        <div class="contact__info">
+          <ContactInfoCard v-for="(data, index) in contacts" :key="index" :data="data" />
+        </div>
+      </div>
+    </BaseContainer>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import { InfoCardProps } from '~/components/contact/InfoCard.vue'
+
+const breadcrumb = [
+  {
+    label: 'Kontakt',
+    link: '/kontakt',
+  },
+]
+
+const contacts: InfoCardProps[] = [
+  { name: 'Biuro', email: 'biuro@***REMOVED***.pl', phones: ['tel.: +48 604 858 045'] },
+  {
+    name: 'Serwis',
+    email: 'serwis@***REMOVED***.pl',
+    phones: ['tel.: +48 61 842-53-00 w.106', 'tel. kom. 604 858 045'],
+  },
+  {
+    name: 'SIECI TELEINFORMATYCZNE',
+    email: 'teleinfo@***REMOVED***.pl',
+    phones: ['tel. kom. 601 777 319'],
+  },
+]
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  @include flex-column;
+  gap: 22px;
+
+  &__header {
+    margin-bottom: 10px;
+    @include flex-column;
+    gap: 8px;
+    padding: 16px;
+    border-radius: 4px;
+    background-color: $gray-color-300;
+  }
+
+  &__title {
+    text-align: left;
+
+    @media ($max-viewport-9) {
+      font-size: rem(20);
+    }
+  }
+
+  &__info {
+    @include flex-row;
+    gap: 10px;
+
+    @media ($max-viewport-9) {
+      flex-direction: column;
+    }
+  }
+}
+</style>
