4198e27be0bb12dd4a403863ade8100fafe6ad91
Maciej Sowiński <maciej.sowinski.work@gmail.com>
2023-05-10 15:06:13 +0200

    feat: create page & add description with banner

---
+++ components/about/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="banner">
+    <BaseContainer class="banner__text-container">
+      <LayoutHeader tag="h2" class="banner__text">
+        Naszą największą siłą jest
+        <span class="banner__text banner__text--color"> doświadczony i zgrany zespół </span>
+        który realizuje zadania z pasją i profesjonalizmem.
+      </LayoutHeader>
+    </BaseContainer>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.banner {
+  width: 100%;
+  background-color: $gray-color-900;
+
+  &__text-container {
+    padding: 36px 0;
+    @include flex-row;
+    justify-content: center;
+  }
+
+  &__text {
+    width: 80%;
+    font-size: rem(30);
+    @media ($max-viewport-9) {
+      font-size: rem(20);
+    }
+
+    &--color {
+      color: var(--secondary-color-alt);
+    }
+  }
+}
+</style>
---
+++ pages/o-nas.vue
@@ -0,0 +1,52 @@
+<template>
+  <NuxtLayout>
+    <LayoutBreadcrumpsProvider :breadcrumbs="breadcrumb" />
+    <div class="about-page">
+      <BaseContainer class="about-page__content about-page__content--narrow">
+        <LayoutHeader variant="black" tag="h1" class="about-page__title">
+          Dowiedz się więcej o naszym zespole
+        </LayoutHeader>
+        <AboutDescription />
+      </BaseContainer>
+
+      <AboutBanner class="about-page__banner" />
+
+      <RentBanner class="about-page__banner" />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const breadcrumb = [
+  {
+    label: 'O nas',
+    link: '/o-nas',
+  },
+]
+</script>
+
+<style lang="scss" scoped>
+.about-page {
+  &__title {
+    text-align: left;
+    font-size: rem(45);
+
+    @media ($max-viewport-9) {
+      font-size: rem(25);
+    }
+  }
+
+  &__content {
+    @include flex-column;
+    gap: 80px;
+
+    &--narrow {
+      gap: 30px;
+    }
+  }
+
+  &__banner {
+    margin: 80px 0;
+  }
+}
+</style>
