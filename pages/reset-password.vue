5e146922038fd6a47c7897bd09f7d316aa2a2185
Mateusz Krasik <mateusz.krasik@escolasoft.com>
2023-05-10 16:16:14 +0200

    feat: reset-password form

--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -1,34 +1,45 @@
 <template>
   <BaseContainer>
-    {{ route.params }}
     <div class="reset-password" @submit.prevent="onSubmit">
-      <LayoutBreadcrumpsProvider :breadcrumbs="[{ label: t('title'), link: '/forgot-password' }]" />
-      <form class="forgot-password__form">
-        <h2 class="forgot-password__header">{{ t('form.header') }}</h2>
-        <span class="forgot-password__description">{{ t('form.description') }}</span>
-        <FormInput
-          v-model="form.values.password"
-          name="email"
-          :label="t('form.email')"
-          rules="required|email"
+      <LayoutBreadcrumpsProvider :breadcrumbs="[]" />
+      <form class="reset-password__form">
+        <h2 class="reset-password__header">{{ t('title') }}</h2>
+        <FormInputPassword v-model="form.values.password" :label="t('password')" name="password" />
+        <FormInputPassword
+          v-model="form.values.confirmPassword"
+          :label="t('confirmPassword')"
+          rules="confirmedPassword:@password"
+          name="confirmPassword"
         />
-        <LayoutButton class="forgot-password__btn" :label="t('form.send')" html-type="submit" />
+        <LayoutButton class="reset-password__btn" :label="t('send')" html-type="submit" />
       </form>
-
-      <NuxtLink :to="'/login'" class="forgot-password__nav">
-        &lt; {{ t('form.backToLogin') }}</NuxtLink
-      >
-    </div></BaseContainer
-  >
+    </div>
+  </BaseContainer>
 </template>
+
+<i18n lang="json">
+{
+  "pl": {
+    "title": "Resetowanie hasła",
+    "password": "Hasło",
+    "confirmPassword": "Powtórz hasło",
+    "send": "Wyślij"
+  }
+}
+</i18n>
 
 <script lang="ts" setup>
 import { useForm } from 'vee-validate'
 
 const t = useLocalI18n()
 const heseya = useHeseya()
-const formatError = useErrorMessage()
 const route = useRoute()
+const router = useRouter()
+
+const query = ref({
+  token: route.query.token as string,
+  email: route.query.email as string,
+})
 
 const form = useForm({
   initialValues: {
@@ -37,13 +48,45 @@ const form = useForm({
   },
 })
 
-const errorMessage = ref<string | null>(null)
-
 const onSubmit = form.handleSubmit(async (values) => {
   try {
-    const { appHost } = useRuntimeConfig()
-  } catch (e: any) {
-    errorMessage.value = formatError(e)
+    await heseya.Auth.resetPassword({
+      token: query.value.token,
+      email: query.value.email,
+      password: values.password,
+    })
+  } finally {
+    router.push('/')
+  }
+})
+
+onBeforeMount(async () => {
+  try {
+    await heseya.Auth.verifyResetPasswordToken(query.value.token, query.value.email)
+  } catch {
+    router.push('/')
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.reset-password {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__form {
+    margin-top: 50px;
+    display: grid;
+    height: 100%;
+    width: 50%;
+    gap: 10px;
+  }
+
+  &__btn {
+    justify-self: center;
+    margin-top: 20px;
+    width: 50%;
+  }
+}
+</style>
