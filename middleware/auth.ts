99b256744351b05e974da14ec1914117c987c000
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-05-16 13:37:53 +0200

    fix: added comment

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -14,6 +14,7 @@ export default defineNuxtRouteMiddleware((to, _from) => {
   const stopWatch = watch(
     () => auth.isLogged,
     () => {
+      // This effect is not cleaned on route change, so we need to check if the route is still the same
       if (!auth.isLogged && route.name === to.name) {
         navigateTo(`/login?redirect=${to.path}`)
         stopWatch()
