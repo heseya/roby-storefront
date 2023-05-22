7d34669b4fd36fb2d0a3c8d0a44777b4f618fff8
Mateusz Krasik <mateusz.krasik@escolasoft.com>
2023-05-22 16:29:06 +0200

    fix: add console warning

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -9,11 +9,6 @@ export type DirectusCollections = {
 export default defineNuxtPlugin((nuxtApp) => {
   const directus = new Directus<DirectusCollections>(nuxtApp.$config.public.directusUrl)
 
-  if (!directus.url) {
-    // eslint-disable-next-line no-console
-    console.warn('Directus does not have URL set up, check DIRECTUS_URL env')
-  }
-
   return {
     provide: { directus },
   }
