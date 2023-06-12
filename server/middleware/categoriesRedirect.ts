0e1a949969e4d0cfa3bc2f7deeef966181190c87
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-06-12 15:17:00 +0200

    chore: added comment

--- server/middleware/categoriesRedirect.ts
+++ server/middleware/categoriesRedirect.ts
@@ -3,6 +3,9 @@ import { convertCategoryUrl } from '../utils/convertCategoryUrl'
 export default defineEventHandler((event) => {
   const url = getRequestURL(event)
 
+  /**
+   * This redirects are needed only for ***REMOVED*** purposes, maybe we can remove it in the future or at least move it to more abstract place?
+   */
   const redirectUrl = convertCategoryUrl(url)
   if (redirectUrl) sendRedirect(event, redirectUrl.toString(), 301)
 })
