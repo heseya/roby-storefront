68dd63e75a69e47d45f40582d2696796f7096e64
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-06-12 11:44:04 +0200

    fix: middleware changes

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,14 @@ export function middleware(request: Request) {
    * In old notation, all subcategries was joined in url using /, now it's using - (its just slug)
    * Also, we need to handle `/page/{number}` on the end of url converting it to `?page={number}`
    */
-  if (url.pathname.startsWith('/kategoria-produktu')) {
-    const slug = url.pathname.split('/').pop()?.replaceAll('/', '-')
+  console.log('Running middleware')
+
+  const matcherPrefix = '/kategoria-produktu/'
+  if (url.pathname.startsWith(matcherPrefix)) {
+    const slug = url.pathname.split(matcherPrefix).pop()?.replaceAll('/', '-')
+    console.log('🚀 ~ file: middleware.ts:14 ~ middleware ~ slug:', slug)
+    console.log('New Url:', new URL(`/category/${slug}`, request.url).toString())
+
     return rewrite(new URL(`/category/${slug}`, request.url))
   }
 }
