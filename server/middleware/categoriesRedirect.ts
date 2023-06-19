58bab9afb26f6cb4d368dfcb4bfd0ee393b50d65
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-06-19 14:15:43 +0200

    fix: test middleware log

--- server/middleware/categoriesRedirect.ts
+++ server/middleware/categoriesRedirect.ts
@@ -2,6 +2,7 @@ import { convertCategoryUrl } from '../utils/convertCategoryUrl'
 
 export default defineEventHandler((event) => {
   const url = getRequestURL(event)
+  console.log('🚀 ~ file: categoriesRedirect.ts:5 ~ defineEventHandler ~ url:', url.href)
 
   /**
    * This redirects are needed only for ***REMOVED*** purposes, maybe we can remove it in the future or at least move it to more abstract place?
