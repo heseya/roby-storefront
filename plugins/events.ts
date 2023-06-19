d36f1433abd060cc753f59a77a9e6026f12f02b1
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-06-19 08:34:03 +0200

    feat: handling events

--- plugins/events.ts
+++ plugins/events.ts
@@ -1,27 +1,106 @@
+import { useGtag } from 'vue-gtag-next'
 import { createHeseyaEventBusService, HeseyaEvent } from '@heseya/store-core'
+import { Pinia } from '@pinia/nuxt/dist/runtime/composables'
 
-export default defineNuxtPlugin((_nuxt) => {
+import { useConfigStore } from '~/store/config'
+import { mapCartItemToItem, mapProductToItem } from '~/utils/google'
+
+export default defineNuxtPlugin((nuxt) => {
+  const gtm = useGtag()
+  const config = useConfigStore(nuxt.$pinia as Pinia)
   const bus = createHeseyaEventBusService()
 
-  bus.on(HeseyaEvent.ViewProduct, (_product) => {})
+  bus.on(HeseyaEvent.ViewProduct, (product) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('view_item', {
+      ecommerce: { items: [mapProductToItem(product)] },
+    })
+  })
 
-  bus.on(HeseyaEvent.ViewProductList, (_data) => {})
+  bus.on(HeseyaEvent.ViewProductList, ({ set, items }) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('view_item_list', {
+      ecommerce: {
+        item_list_name: set?.name,
+        items: items.map(mapProductToItem),
+      },
+    })
+  })
 
-  bus.on(HeseyaEvent.AddToCart, (_item) => {})
+  bus.on(HeseyaEvent.AddToCart, (item) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('add_to_cart', {
+      ecommerce: {
+        currency: config.currency,
+        value: item.price,
+        items: [mapCartItemToItem(item)],
+      },
+    })
+  })
 
-  bus.on(HeseyaEvent.RemoveFromCart, (_item) => {})
+  bus.on(HeseyaEvent.RemoveFromCart, (item) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('remove_from_cart', {
+      ecommerce: {
+        currency: config.currency,
+        value: item.price,
+        items: [mapCartItemToItem(item)],
+      },
+    })
+  })
 
-  bus.on(HeseyaEvent.AddShippingInfo, (_data) => {})
+  bus.on(HeseyaEvent.AddShippingInfo, ({ shipping, items }) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('add_shipping_info', {
+      ecommerce: {
+        currency: config.currency,
+        value: shipping.price,
+        items: items.map(mapCartItemToItem),
+      },
+    })
+  })
 
-  bus.on(HeseyaEvent.InitiateCheckout, (_items) => {})
+  bus.on(HeseyaEvent.InitiateCheckout, (items) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('begin_checkout', {
+      ecommerce: { items: items.map(mapCartItemToItem) },
+    })
+  })
 
-  bus.on(HeseyaEvent.Login, () => {})
+  bus.on(HeseyaEvent.Login, () => {
+    gtm.event('login', {
+      method: 'email',
+    })
+  })
 
-  bus.on(HeseyaEvent.Register, () => {})
+  bus.on(HeseyaEvent.Register, () => {
+    gtm.event('sign_up', {
+      method: 'email',
+    })
+  })
 
-  bus.on(HeseyaEvent.Purchase, (_data) => {})
+  bus.on(HeseyaEvent.Purchase, ({ order, items }) => {
+    gtm.event('', { ecommerce: null })
+    // TODO: add coupons?
+    gtm.event('purchase', {
+      ecommerce: {
+        transaction_id: order.code,
+        affiliation: 'Ksiażki.pl website',
+        value: order.summary,
+        currency: config.currency,
+        shipping: order.shipping_price,
+        items: items.map(mapCartItemToItem),
+        items_value: order.cart_total,
+      },
+    })
+  })
 
-  bus.on(HeseyaEvent.ViewCart, (_items) => {})
+  bus.on(HeseyaEvent.ViewCart, (items) => {
+    gtm.event('', { ecommerce: null })
+    gtm.event('view_cart', {
+      ecommerce: { items: items.map(mapCartItemToItem) },
+    })
+  })
 
   return {
     provide: {
