0a439b11012ae51403413844f01fe0467e52a411
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-06-27 07:38:23 +0200

    test: show debug events when no prod

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,13 +1,14 @@
 import VueGtag, { trackRouter } from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const { googleTagManagerId } = usePublicRuntimeConfig()
+  const { googleTagManagerId, isProduction } = usePublicRuntimeConfig()
   if (!googleTagManagerId) return
 
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: googleTagManagerId,
     },
+    useDebugger: !isProduction,
   })
 
   trackRouter(useRouter())
