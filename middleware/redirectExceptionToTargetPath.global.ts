905a40a7cb24707dfb18bc11cc69f6651ff95b7d
Mateusz Krasik <mateusz.krasik@escolasoft.com>
2023-07-04 11:27:03 +0200

    fix: add comment for redirect middleware

--- middleware/redirectExceptionToTargetPath.global.ts
+++ middleware/redirectExceptionToTargetPath.global.ts
@@ -1,3 +1,4 @@
+// temporary solution for ***REMOVED***
 export default defineNuxtRouteMiddleware((to, _from) => {
   if (to.path.startsWith('/tag-produktu/')) {
     return navigateTo(to.path.replace('/tag-produktu/', '/category/'))
