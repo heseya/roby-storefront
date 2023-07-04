35b061b970c4149088bf739542281a577586d70d
Mateusz Krasik <mateusz.krasik@escolasoft.com>
2023-07-04 10:12:03 +0200

    feat: add redirect middleware

---
+++ middleware/redirectExceptionToTargetPath.global.ts
@@ -0,0 +1,5 @@
+export default defineNuxtRouteMiddleware((to, _from) => {
+  if (to.path.startsWith('/tag-produktu/')) {
+    return navigateTo(to.path.replace('/tag-produktu/', '/category/'))
+  }
+})
