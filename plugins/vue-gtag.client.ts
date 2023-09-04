ffb34a6495915abed775884bf4c3f2b8e3136ff5
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-09-04 14:55:58 +0200

    feat: use cookies to disable gtag ads

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -1,4 +1,10 @@
-import VueGtag, { trackRouter } from 'vue-gtag-next'
+import VueGtag, { trackRouter, useGtag } from 'vue-gtag-next'
+import { COOKIE_ACCEPTED_KEY } from '@/consts/cookiesKeys'
+
+const COOKIES_CONFIG = {
+  maxAge: 365 * 24 * 60 * 60,
+  path: '/',
+} as const
 
 export default defineNuxtPlugin((nuxtApp) => {
   const { googleTagManagerId, isProduction } = usePublicRuntimeConfig()
@@ -10,6 +16,30 @@ export default defineNuxtPlugin((nuxtApp) => {
     },
     useDebugger: !isProduction,
   })
+
+  const { set } = useGtag()
+
+  const marketingCookie = useStatefulCookie<number>(COOKIE_ACCEPTED_KEY, COOKIES_CONFIG)
+  const analyticsCookie = useStatefulCookie<number>(COOKIE_ACCEPTED_KEY, COOKIES_CONFIG)
+
+  watch(
+    marketingCookie,
+    (value) => {
+      // TODO: is this correct?
+      if (value === 0) set({ allow_ad_personalization_signals: false, allow_google_signals: false })
+    },
+    { immediate: true },
+  )
+
+  watch(
+    analyticsCookie,
+    (value) => {
+      // TODO: what should be enabled/disabled here?
+      if (value) set({})
+      else set({})
+    },
+    { immediate: true },
+  )
 
   trackRouter(useRouter())
 })
