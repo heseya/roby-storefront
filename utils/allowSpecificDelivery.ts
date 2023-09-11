92b470fef63ec13108cdfa3b49ac03278338dfb3
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-09-11 09:22:39 +0200

    feat: paczkomat is once again enabled by default

--- utils/allowSpecificDelivery.ts
+++
@@ -1,6 +0,0 @@
-import { CartItem } from '@heseya/store-core'
-
-export const allowSpecificDelivery = (items: CartItem[], fieldName: string): boolean => {
-  // TODO: field product in item exists but its private, we need to fix it in the future
-  return items.every((item) => item.product.metadata?.[fieldName] ?? false)
-}
