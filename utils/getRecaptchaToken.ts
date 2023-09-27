76dd982b9e494a5e0d3f0ae9297ff4272b58dbe9
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-09-27 10:09:03 +0200

    fix: handle when no recaptcha key

--- utils/getRecaptchaToken.ts
+++ utils/getRecaptchaToken.ts
@@ -9,6 +9,7 @@ declare global {
 
 export const getRecaptchaToken = (publicToken: string): Promise<string> => {
   if (!window.grecaptcha) return Promise.reject(new Error('Recaptcha not loaded'))
+  if (!publicToken) return Promise.reject(new Error('Recaptcha public token not provided'))
 
   return new Promise((resolve, reject) => {
     window.grecaptcha.ready(function () {
