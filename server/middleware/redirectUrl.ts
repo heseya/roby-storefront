8fe3de3a34d2cb87431d60f849e3e31635e6b0b9
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-10-02 09:04:44 +0200

    fix: redirect tweak

--- server/middleware/redirectUrl.ts
+++ server/middleware/redirectUrl.ts
@@ -3,7 +3,7 @@ import { redirectUrl } from '../utils/redirectUrl'
 export default defineEventHandler((event) => {
   const url = getRequestURL(event)
 
-  redirectUrl(url).then((redirect) => {
+  return redirectUrl(url).then((redirect) => {
     if (redirect?.target !== '') {
       return sendRedirect(event, redirect.target, redirect.type)
     }
