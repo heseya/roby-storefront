3e10dc0727dcb7c66e3c4d03cea2785687a42390
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-10-13 08:59:06 +0200

    Revert "feat: disabled zip validation"
    This reverts commit eb62335e2d4aa59a64a511d04e6c54ac5ee685c8.

--- plugins/vee-validate.client.ts
+++ plugins/vee-validate.client.ts
@@ -28,10 +28,8 @@ export default defineNuxtPlugin((nuxt) => {
   defineRule('plVatNumber', (value: string) => {
     return /^(PL)?[0-9]{10}$/gm.test(value)
   })
-  defineRule('plZip', (_value: string): boolean => {
-    // TODO: disabled vat number validation due to the ST-131 error
-    return true
-    // return /^[0-9]{2}-[0-9]{3}$/gm.test(value)
+  defineRule('plZip', (value: string) => {
+    return /^[0-9]{2}-[0-9]{3}$/gm.test(value)
   })
   defineRule('max', max)
 
@@ -49,7 +47,7 @@ export default defineNuxtPlugin((nuxt) => {
           phone: 'Numer telefonu jest niepoprawny',
           vatNumber: 'Podany numer NIP jest nieprawidłowy',
           plVatNumber: 'Podany numer NIP jest nieprawidłowy',
-          plZip: 'Kod pocztowy jest nieprawidłowy. Powinien być posiadać format "XX-XXX".',
+          plZip: 'Kod pocztowy jest nieprawidłowy',
           max: 'Przekroczono maksymalną ilość znaków. Dopuszczalna liczba znaków to 0:{max}',
         },
       },
@@ -65,7 +63,7 @@ export default defineNuxtPlugin((nuxt) => {
           phone: 'The phone number is incorrect',
           vatNumber: 'Provided VAT Number is incorrect',
           plVatNumber: 'Provided VAT Number is incorrect',
-          plZip: 'Zip code is incorrect. It should have the format "XX-XXX".',
+          plZip: 'Zip code is incorrect',
           max: 'The maximum number of characters has been exceeded. The allowed number of characters is 0:{max}',
         },
       },
