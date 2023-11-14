7c070625345bbf519875646a1087c93725099b54
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-11-14 10:11:42 +0100

    fix: ekomi tweaks

--- components/integration/ekomi/Widget.vue
+++ components/integration/ekomi/Widget.vue
@@ -10,11 +10,44 @@
 const { ekomiCustomerId } = usePublicRuntimeConfig()
 const props = defineProps<{ token: string }>()
 
+if (ekomiCustomerId)
+  useHead({
+    script: [
+      {
+        hid: 'ekomi-script',
+        id: 'ekomi',
+        children: `
+        function registerWidget (w, token) {
+          w['_ekomiWidgetsServerUrl'] = 'https://widgets.ekomi.com';
+          w['_customerId'] = ${ekomiCustomerId};
+          if (w['_language'] == undefined) {
+              w['_language'] = new Array();
+          }
+          w['_language'][token] = 'auto';
+          if(typeof(w['_ekomiWidgetTokens']) !== 'undefined'){
+              w['_ekomiWidgetTokens'][w['_ekomiWidgetTokens'].length] = token;
+          } else {
+              w['_ekomiWidgetTokens'] = new Array(token);
+          }
+          if(typeof(ekomiWidgetJs) == 'undefined') {
+              ekomiWidgetJs = true;
+              var scr = document.createElement('script');scr.src = 'https://sw-assets.ekomiapps.de/static_resources/widget.js';
+              var head = document.getElementsByTagName('head')[0];head.appendChild(scr);
+          } else {if (typeof ekomiWidgetMain != 'undefined') {ekomiWidgetMain('ajax', token);}}
+          return true;
+      }
+        `,
+      },
+    ],
+  })
+
 onMounted(() => {
   if (!ekomiCustomerId) return
 
   setTimeout(() => {
-    // @ts-ignore
+    // eslint-disable-next-line no-console
+    if (!window.registerWidget) console.error(`[${props.token}] Ekomi widget not loaded!`)
+
     window.registerWidget?.(window, props.token)
   }, 2000)
 })
