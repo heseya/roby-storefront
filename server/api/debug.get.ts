5797ba92f36be4f7aece8bb7c1b875ea7249c6d8
Artur Mędrygał <artur.medrygal@escolasoft.com>
2024-02-27 12:51:59 +0100

    chore: removed debug pages

--- server/api/debug.get.ts
+++
@@ -1,5 +0,0 @@
-// TODO: remove this file
-export default defineEventHandler((event) => {
-  // @ts-ignore Docs suggest to pass event to useRuntimeConfig, but it's not typed? https://nuxt.com/docs/guide/going-further/runtime-config#server-routes
-  return useRuntimeConfig(event)
-})
